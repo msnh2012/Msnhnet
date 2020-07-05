91826998f1d54c4bd82c01cb67b245b3ad80bd48
msnh <wwwtyyz@outlook.com>
2020-07-05 18:51:54 +0800

    Add files via upload

---
+++ pytorch2msnhbin/pytorch2msnhbin.py
@@ -0,0 +1,25 @@
+import torchvision.models as models
+import torch
+from torchsummary import summary
+from struct import pack
+
+md = models.alexnet(pretrained = True)
+md.to("cpu")
+md.eval()
+
+print(md, file = open("net.txt", "a"))
+
+summary(md, (3, 224, 224),device='cpu')
+
+val = []
+dd = 0
+for name in md.state_dict():
+        if "num_batches_tracked" not in name:
+                c = md.state_dict()[name].data.flatten().numpy().tolist()
+                dd = dd + len(c)
+                print(name, ":", len(c))
+                val.extend(c)
+
+with open("alexnet.msnhbin","wb") as f:
+    for i in val :
+        f.write(pack('f',i))
