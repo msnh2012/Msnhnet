29d0bcc8d69caccea713d9763c27ce90b78bf8a0
BBuf <1182563586@qq.com>
2020-08-21 12:44:12 +0800

    Add conv3x3s2

--- include/Msnhnet/layers/arm/MsnhConvolution3x3s2.h
+++ include/Msnhnet/layers/arm/MsnhConvolution3x3s2.h
@@ -1,14 +1,12 @@
-//
-
 namespace Msnhnet
 {
 
-class ConvolutionalLayerArm3x3s1
+class ConvolutionalLayerArm3x3s2
 {
 public:
     //bottom: src, inWidth, inHeight, inChannel
     //top: dest, outWidth, outHeight, outChannel
-    void conv3x3s1_neon(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, float *const &kernel, 
+    void conv3x3s2_neon(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, float *const &kernel, 
                                         float* &dest, const int &outWidth, const int &outHeight, const int &outChannel);
 };
 
---
+++ src/layers/arm/MsnhConvolution3x3s2.cpp
@@ -0,0 +1,10 @@
+#include "Msnhnet/layers/arm/MsnhConvolution3x3s2.h"
+#include "Msnhnet/config/MsnhnetCfg.h"
+
+namespace Msnhnet
+{
+    void ConvolutionalLayerArm3x3s2::conv3x3s2_neon(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, float *const &kernel, 
+                                        float* &dest, const int &outWidth, const int &outHeight, const int &outChannel){
+        
+    }
+}
