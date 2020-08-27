4ae055e1155fe3af433c18e0ae8b088d51d4262a
BBuf <1182563586@qq.com>
2020-08-27 17:46:39 +0800

    Add BatchNorm Assembly

--- include/Msnhnet/layers/arm/MsnhBatchNorm.h
+++ include/Msnhnet/layers/arm/MsnhBatchNorm.h
@@ -5,7 +5,7 @@ namespace Msnhnet
 class BatchNormLayerArm
 {
 public:
-    void BatchNorm(float * &src, const int &inWidth, const int &inHeight,  const int &inChannel, 
+    void BatchNorm(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, float* &dest,
                     float *const &Scales, float *const &rollMean, float *const &rollVariance, float *const &biases);
 };
 
--- src/layers/arm/MsnhBatchNorm.cpp
+++ src/layers/arm/MsnhBatchNorm.cpp
@@ -3,7 +3,7 @@
 
 namespace Msnhnet
 {
-    void BatchNormLayerArm::BatchNorm(float * &src, const int &inWidth, const int &inHeight,  const int &inChannel,
+    void BatchNormLayerArm::BatchNorm(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, float* &dest,
                     float *const &Scales, float *const &rollMean, float *const &rollVariance, float *const &biases){
         // a = bias - slope * mean / sqrt(var)
         // b = slope / sqrt(var)
@@ -13,9 +13,9 @@ namespace Msnhnet
 #endif
 
         int in_size = inWidth * inHeight;
-        float *srcPtr = src;
+        const float *srcPtr = src;
+        float *destPtr = dest;
         int nn, remain;
-        cout << inChannel << " " << in_size << endl;
         for(int i = 0; i < inChannel; i++){
 
             float sqrtVar = sqrt(rollVariance[i] + 0.00001f);
@@ -32,25 +32,56 @@ namespace Msnhnet
             #endif    
 
             #if USE_NEON
-                for(; nn > 0; nn--){
+                // for(; nn > 0; nn--){
+                //     #if __aarch64__
+                //         throw Exception(1, "Error: armv8 temporarily not supported!", __FILE__, __LINE__, __FUNCTION__);
+                //     #else
+                //         float32x4_t tmp = vld1q_f32(srcPtr);
+                //         float32x4_t sum = vmulq_f32(tmp, b_new);
+                //         sum = vaddq_f32(sum, a_new);
+                //         vst1q_f32(destPtr, sum);
+                //     #endif 
+                //     srcPtr += 4;
+                //     destPtr += 4;
+                // }
+
+                if(nn > 0){
                     #if __aarch64__
-                        throw Exception(1, "Error: armv8 temporarily not supported!", __FILE__, __LINE__, __FUNCTION__);
                     #else
-                        float32x4_t tmp = vld1q_f32(srcPtr);
-                        float32x4_t sum = vmulq_f32(tmp, b_new);
-                        sum = vaddq_f32(sum, a_new);
-                        vst1q_f32(srcPtr, sum);
-                        srcPtr += 4;
-                    #endif 
+                        asm volatile(
+                            "vdup.f32   q0, %6              \n"
+                            "vdup.f32   q1, %7              \n"
+
+                            "0:                             \n"
+                            "pld        [%2, #128]          \n"
+                            "vld1.f32   {d4,d5}, [%1]!      \n"
+
+                            "vmul.f32   q3, q2, q1          \n"
+                            "vadd.f32   q3, q3, q0          \n"
+
+                            "vst1.f32   {d6-d7}, [%2]!    \n"
+
+                            : "=r"(nn),     // %0
+                            "=r"(srcPtr), // %1
+                            "=r"(destPtr),     // %2
+                            : "=r"(nn),     // %0
+                            "=r"(srcPtr), // %4
+                            "=r"(destPtr),     // %5
+                            "w"(a_new), // %6
+                            "w"(b_new), // %7
+                            : "cc", "memory", "q0", "q1", "q2", "q3");
+                        );
+                    #endif
                 }
+
             #endif
 
             for(; remain > 0; remain--){
-                *srcPtr = b * (*srcPtr) + a;
+                *destPtr = b * (*srcPtr) + a;
                 srcPtr++;
+                destPtr++;
             }
         }
-
     }
 
 }
