aeca3b667b1a1012c83fac487639eb666535968c
BBuf <1182563586@qq.com>
2020-08-30 22:16:51 +0800

    Update ActivationLayerArm

--- src/layers/arm/MsnhActivationLayerArm.cpp
+++ src/layers/arm/MsnhActivationLayerArm.cpp
@@ -37,12 +37,14 @@ void ActivationLayerArm::reluActivate(float * &src, const int &inWidth, const in
                 #else
                     asm volatile(
                     "veor       q1, q0, q0          \n" //模拟0，more speed
+                    
                     "0:                             \n"
                     "pld        [%1, #128]          \n"
                     "vld1.f32   {d0-d1}, [%1 :128]  \n"
                     "vmax.f32   q0, q0, q1          \n"
-                    "subs       %0, #1              \n"
                     "vst1.f32   {d0-d1}, [%1 :128]! \n"
+
+                    "subs       %0, #1              \n"
                     "bne        0b                  \n"
                     : "=r"(nn), // %0
                     "=r"(srcPtr) // %1
@@ -61,7 +63,59 @@ void ActivationLayerArm::reluActivate(float * &src, const int &inWidth, const in
 }
 
 void ActivationLayerArm::relu6Activate(float * &src, const int &inWidth, const int &inHeight,  const int &inChannel){
+    int in_size = inWidth * inHeight;
+    int nn, remain;
 
+    const float six = 6.0;
+
+    #if USE_OMP
+    #pragma omp parallel for num_threads(OMP_THREAD)
+#endif
+    for(int i = 0; i < inChannel; i++){
+
+        float *srcPtr = src + i * in_size;
+
+        #if USE_NEON
+                nn = in_size >> 2;
+                remain = in_size - nn << 2;
+        #else
+                remain = in_size;
+        #endif
+
+        #if USE_NEON
+            if(nn > 0){
+                #if __aarch64__
+                    throw Exception(1, "Error: armv8 temporarily not supported!", __FILE__, __LINE__, __FUNCTION__);
+                #else
+                    asm volatile(
+                    "veor       q1, q0, q0          \n" //模拟0，more speed
+                    "vdup.f32   q2, %4              \n"
+
+                    "0:                             \n"
+                    "pld        [%1, #128]          \n"
+                    "vld1.f32   {d0-d1}, [%1 :128]  \n"
+                    "vmax.f32   q0, q0, q1          \n"
+                    "vmin.f32   q0, q0, q2          \n"
+
+                    "vst1.f32   {d0-d1}, [%1 :128]! \n"
+
+                    "subs       %0, #1              \n"
+                    "bne        0b                  \n"
+                    : "=r"(nn), // %0
+                    "=r"(srcPtr) // %1
+                    : "0"(nn),
+                    "1"(srcPtr)
+                    "r"(six) // %4
+                    : "cc", "memory", "q0", "q1");
+                #endif
+            }
+        #endif
+
+        for(; remain > 0; remain--){
+            *srcPtr = std::min(std::max(*srcPtr, 0.f), 6.0);
+            srcPtr++;
+        }
+    }
 }
 
 void ActivationLayerArm::eluActivate(float * &src, const int &inWidth, const int &inHeight,  const int &inChannel){
@@ -81,7 +135,65 @@ void ActivationLayerArm::rampActivate(float * &src, const int &inWidth, const in
 }
 
 void ActivationLayerArm::leakyActivate(float * &src, const int &inWidth, const int &inHeight,  const int &inChannel, const float &params){
+    int in_size = inWidth * inHeight;
+    int nn, remain;
 
+    #if USE_OMP
+    #pragma omp parallel for num_threads(OMP_THREAD)
+#endif
+    for(int i = 0; i < inChannel; i++){
+
+        float *srcPtr = src + i * in_size;
+
+        #if USE_NEON
+                nn = in_size >> 2;
+                remain = in_size - nn << 2;
+        #else
+                remain = in_size;
+        #endif
+
+        #if USE_NEON
+            if(nn > 0){
+                #if __aarch64__
+                    throw Exception(1, "Error: armv8 temporarily not supported!", __FILE__, __LINE__, __FUNCTION__);
+                #else
+                    asm volatile(
+                    "veor       q1, q0, q0          \n"
+                    // slope
+                    "vdup.f32   q2, %4              \n"
+                    
+                    "0:                             \n"
+                    "pld        [%1, #128]          \n"
+                    "vld1.f32   {d0-d1}, [%1 :128]  \n"
+
+                    // r[i] = a[i] <= b[i] ? 1: 0
+                    "vcl e.f32   q3, q0, q1          \n"
+                    // 
+                    "vmul.f32   q4, q0, q2          \n"
+                    // _p = vbslq_f32(_lemask, _ps, _p);
+                    "vbit.32    q0, q4, q3          \n"
+
+                    "vst1.f32   {d0-d1}, [%1 :128]! \n"
+
+                    "subs       %0, #1              \n"
+                    "bne        0b                  \n"
+                    : "=r"(nn), // %0
+                    "=r"(srcPtr) // %1
+                    : "0"(nn),
+                    "1"(srcPtr),
+                    "r"(params) // %4
+                    : "cc", "memory", "q0", "q1", "q2", "q3", "q4");
+                #endif
+            }
+        #endif
+
+        for(; remain > 0; remain--){
+            if((*srcPtr) < 0){
+                *srcPtr *= params;
+            }
+            srcPtr++;
+        }
+    }
 }
 
 void ActivationLayerArm::tanhActivate(float * &src, const int &inWidth, const int &inHeight,  const int &inChannel){
