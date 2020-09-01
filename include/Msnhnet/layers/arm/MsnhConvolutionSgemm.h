74a03bbe81046fdce02d61bfaf4fa33da6fc7bc2
BBuf <1182563586@qq.com>
2020-09-01 20:32:33 +0800

    Add MsnhConvolutionSgemm

--- include/Msnhnet/layers/arm/MsnhConvolutionSgemm.h
+++ include/Msnhnet/layers/arm/MsnhConvolutionSgemm.h
@@ -9,7 +9,8 @@ public:
     //bottom: src, inWidth, inHeight, inChannel
     //top: dest, outWidth, outHeight, outChannel
     void convolution_im2col_sgemm(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, float *const &kernel,
-                            const int &kernelW, const int &kernelH,int float* &dest, const int &outWidth, const int &outHeight, const int &outChannel);
+                            const int &kernelW, const int &kernelH, float* &dest, const int &outWidth, const int &outHeight, const int &outChannel,
+                            const int& StrideH, const int &StrideW);
 };
 
 }
--- src/layers/arm/MsnhConvolutionSgemm.cpp
+++ src/layers/arm/MsnhConvolutionSgemm.cpp
@@ -4,14 +4,41 @@
 namespace Msnhnet
 {
     void ConvolutionLayerSgemm::convolution_im2col_sgemm(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, float *const &kernel,
-                            const int &kernelW, const int &kernelH,int float* &dest, const int &outWidth, const int &outHeight, const int &outChannel){
+                            const int &kernelW, const int &kernelH, float* &dest, const int &outWidth, const int &outHeight, const int &outChannel, 
+                            const int& StrideH, const int &StrideW){
         // 1. im2col
 
         float *src_im2col = new float[outWidth * outHeight * kernelH * kernelW * inChannel];
         
         const int stride = kernelW * kernelH * outHeight * outWidth;
+        const int in_size = inHeight * inWidth;
 
-        
+    // inCahnnel x kW x kH
+    // outWidth x outHeight
+
+#if USE_OMP
+    #pragma omp parallel for num_threads(OMP_THREAD)
+#endif
+
+        for(int cc = 0; cc < inChannel; cc++){
+            const float *src0 = src + cc * in_size;
+            int dst_idx = stride * cc;
+            for(int i = 0; i < kernelH; i++){
+                for(int j = 0; j < kernelW; j++){
+                    for(int x = 0; x < outHeight; x++){
+                        for(int y = 0; y < outWidth; y++){
+                            int row = x * StrideH + i;
+                            int col = y * StrideW + j;
+                            int ori_idx = row * inWidth + col;
+                            src_im2col[dst_idx] = src0[ori_idx];
+                            dst_idx++;      
+                        }
+                    }
+                }
+            }
+        }
+
+
 
     }
 }
