7f5adbf0bceba6b317b1db44bfe86133315b7635
BBuf <1182563586@qq.com>
2020-09-12 15:12:43 +0800

    Fix ReLU6 Bus Error BUG

--- src/layers/arm/MsnhActivationLayerArm.cpp
+++ src/layers/arm/MsnhActivationLayerArm.cpp
@@ -24,7 +24,7 @@ void ActivationLayerArm::reluActivate(float * &src, const int &inWidth, const in
 
         #if USE_NEON
                 int nn = in_size >> 2;
-                int remain = nn << 2;
+                int remain = in_size - (nn << 2);
         #else
                 int remain = in_size;
         #endif
@@ -66,7 +66,7 @@ void ActivationLayerArm::relu6Activate(float * &src, const int &inWidth, const i
 
     const float six = 6.0;
 
-    #if USE_OMP
+#if USE_OMP
     #pragma omp parallel for num_threads(OMP_THREAD)
 #endif
     for(int i = 0; i < inChannel; i++){
@@ -75,7 +75,7 @@ void ActivationLayerArm::relu6Activate(float * &src, const int &inWidth, const i
 
         #if USE_NEON
                 int nn = in_size >> 2;
-                int remain = in_size - nn << 2;
+                int remain = in_size - (nn << 2);
         #else
                 int remain = in_size;
         #endif
@@ -86,23 +86,23 @@ void ActivationLayerArm::relu6Activate(float * &src, const int &inWidth, const i
                     throw Exception(1, "Error: armv8 temporarily not supported!", __FILE__, __LINE__, __FUNCTION__);
                 #else
                     asm volatile(
-                    "veor       q1, q0, q0          \n" //模拟0，more speed
+                    "veor       q1, q0, q0          \n" 
                     "vdup.f32   q2, %4              \n"
 
                     "0:                             \n"
                     "pld        [%1, #128]          \n"
-                    "vld1.f32   {d0-d1}, [%1 :128]  \n"
+                    "vld1.f32   {d0-d1}, [%1]  \n"
                     "vmax.f32   q0, q0, q1          \n"
                     "vmin.f32   q0, q0, q2          \n"
 
-                    "vst1.f32   {d0-d1}, [%1 :128]! \n"
+                    "vst1.f32   {d0-d1}, [%1]! \n"
 
                     "subs       %0, #1              \n"
                     "bne        0b                  \n"
                     : "=r"(nn), // %0
                     "=r"(srcPtr) // %1
                     : "0"(nn),
-                    "1"(srcPtr)
+                    "1"(srcPtr),
                     "r"(six) // %4
                     : "cc", "memory", "q0", "q1");
                 #endif
@@ -110,7 +110,7 @@ void ActivationLayerArm::relu6Activate(float * &src, const int &inWidth, const i
         #endif
 
         for(; remain > 0; remain--){
-            *srcPtr = std::min(std::max(*srcPtr, 0.f), 6.0);
+            *srcPtr = std::min(std::max(*srcPtr, 0.f), six);
             srcPtr++;
         }
     }
