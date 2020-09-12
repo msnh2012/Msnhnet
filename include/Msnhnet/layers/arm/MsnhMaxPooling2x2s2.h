676ac7d539d970eca5095944a9f485b1267d063e
BBuf <1182563586@qq.com>
2020-09-12 22:14:14 +0800

    Add MaxPool2D neon

--- include/Msnhnet/layers/arm/MsnhMaxPooling2x2s2.h
+++ include/Msnhnet/layers/arm/MsnhMaxPooling2x2s2.h
@@ -1,13 +1,14 @@
 namespace Msnhnet
 {
 
-class MaxPooling2x2Arm
+class MaxPooling2x2s2Arm
 {
 public:
     //bottom: src, inWidth, inHeight, inChannel
     //top: dest, outWidth, outHeight, outChannel
+    //ceil mode如果为1并且特征图长或宽为奇数，提前执行Padding一圈0即可
     void pooling(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, 
-                                        float* &dest, const int& outWidth, const int& outHeight, const int& outChannel);
+                                    float* &dest, const int& ceilModel);
 };
 
 }
---
+++ src/layers/arm/MsnhMaxPooling2x2s2.cpp
@@ -0,0 +1,87 @@
+#include "Msnhnet/layers/arm/MsnhPadding.h"
+#include "Msnhnet/layers/arm/MsnhMaxPooling2x2s2.h"
+#include "Msnhnet/config/MsnhnetCfg.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+namespace Msnhnet
+{
+
+
+//bottom: src, inWidth, inHeight, inChannel
+//top: dest, outWidth, outHeight, outChannel
+void MaxPooling2x2s2Arm::pooling(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, 
+                                    float* &dest, const int& ceilModel){
+        const int outHeight = inHeight >> 1;
+        const int outWidth = inWidth >> 1;
+        const int inSize = inHeight * inWidth;
+        const int outSize = outHeight * outWidth;
+
+#if USE_OMP
+    #pragma omp parallel for num_threads(OMP_THREAD)
+#endif
+        for(int c = 0; c < inChannel; c++){
+            const float *srcptr = src + c * inSize;
+            float *destptr = dest + c * outSize;
+            const float* r0 = srcptr;
+            const float* r1 = srcptr + inWidth;
+            for(int i = 0; i < outHeight; i++){
+#if USE_NEON
+                int nn = outWidth >> 2;
+                int remain = outWidth - (nn << 2);
+#else
+                int remain = outWidth;
+#endif
+
+                if(nn > 0){
+#if __aarch64__
+                    throw Exception(1, "Error: armv8 temporarily not supported!", __FILE__, __LINE__, __FUNCTION__);
+#else
+                    asm volatile(
+                        "0:                             \n"
+                        "pld        [%1, #256]          \n"
+                        "pld        [%2, #256]          \n"
+                        //q0,q1 = [d0, d1, d2, d3] = [a, b, c, d]
+                        "vld1.f32   {d0-d3}, [%1]!      \n"
+                        //q2,d3 = [d4, d5, d6, d7] = [e, f, g, h]
+                        "vld1.f32   {d4-d7}, [%2]!      \n"
+                        //q0 = [max(a, e), max(b, f)]
+                        "vmax.f32   q0, q0, q2          \n"
+                        "vmax.f32   q1, q1, q3          \n"
+                        //d4 = max([max(a, e), max(b, f)])
+                        "vpmax.f32  d4, d0, d1          \n"
+                        "vpmax.f32  d5, d2, d3          \n"
+                        "subs       %0, #1              \n"
+                        "vst1.f32   {d4-d5}, [%3]!      \n"
+                        "bne        0b                  \n"
+                        : "=r"(nn),    // %0
+                        "=r"(r0),    // %1
+                        "=r"(r1),    // %2
+                        "=r"(destptr) // %3
+                        : "0"(nn),
+                        "1"(r0),
+                        "2"(r1),
+                        "3"(destptr)
+                        : "cc", "memory", "q0", "q1", "q2", "q3");
+#endif
+                }
+
+                for(; remain > 0; remain--){
+                    float mx0 = std::max(r0[0], r0[1]);
+                    float mx1 = std::max(r1[0], r1[1]);
+                    *destptr = std::max(mx0, mx1);
+                    r0 += 2;
+                    r1 += 2;
+                    destptr++;
+                }
+
+                r0 += 2 * (inWidth - outWidth);
+                r1 += 2 * (inWidth - outWidth);
+            }
+
+        }
+    }
+
+
+}
