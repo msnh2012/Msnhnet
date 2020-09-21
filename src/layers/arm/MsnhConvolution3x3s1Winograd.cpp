c97e6f71579edfe7e3f79dea1377ed515b273bc1
BBuf <1182563586@qq.com>
2020-09-21 23:44:05 +0800

    Add Arm Winograd3x3s1

--- src/layers/arm/MsnhConvolution3x3s1Winograd.cpp
+++ src/layers/arm/MsnhConvolution3x3s1Winograd.cpp
@@ -1,6 +1,7 @@
 #define USE_ARM 1
 #ifdef USE_ARM
 #include "Msnhnet/layers/arm/MsnhConvolution3x3s1Winograd.h"
+#include "Msnhnet/layers/arm/MsnhPadding.h"
 #include "Msnhnet/config/MsnhnetCfg.h"
 
 namespace Msnhnet
@@ -20,6 +21,7 @@ namespace Msnhnet
             {0.0f, 0.0f, 1.0f}
         };
 
+        const int kernelTmSize = inChannel * 8 * 8;
 
 #if USE_OMP
     #pragma omp parallel for num_threads(OMP_THREAD)
@@ -34,16 +36,161 @@ namespace Msnhnet
                 const float* k1 = kernel0 + 3;
                 const float* k2 = kernel0 + 6;
 
-                
+                float tmp[8][3];    // tmp = G*g
+                for(int i = 0; i < 8; i++){
+                    tmp[i][0] = k0[0] * ktm[i][0] + k0[1] * ktm[i][1] + k0[2] * ktm[i][2];
+                    tmp[i][1] = k1[0] * ktm[i][0] + k1[1] * ktm[i][1] + k1[2] * ktm[i][2];
+                    tmp[i][2] = k2[0] * ktm[i][0] + k2[1] * ktm[i][1] + k2[2] * ktm[i][2];
+                }
+
+                //U = kernel_tm0 = G*g*G^T
+                //[8*3] x [3*8]
+                for(int i = 0; i < 8; i++){
+                    float *tmpPtr = &tmp[i][0];
+                    for(int j = 0; j < 8; j++){
+                        kernel_tm0[i * 8 + j] = tmpPtr[0] * ktm[j][0] + tmpPtr[1] * ktm[j][1] + tmpPtr[2] * ktm[j][2];
+                    }
+                }
 
             }
         }
 
+        int nnOutchannel = outChannel >> 2;
+        int remainOutChannel = nnOutchannel << 2;
+        
+        int packOutChannel = nnOutchannel + (outChannel % 4 + 3) / 4);
+        int packOutH = 1;
+        int packOutW = (8 * 8 * inChannel * 4);
+
+        float *kernel_tm2 = new float[packOutChannel * packOutH * packOutW];
+
+#if USE_OMP
+    #pragma omp parallel for num_threads(OMP_THREAD)
+#endif       
+        for(int cc = 0; cc < nnOutchannel; cc++){
+            int c = cc << 2;
+            float *ktm2 = kernel_tm2 + cc * packOutH * packOutW;
+            
+            const float *kernel0_tm = kernel_tm + cc * kernelTmSize;
+            const float *kernel1_tm = kernel_tm + (cc + 1) * kernelTmSize;
+            const float *kernel2_tm = kernel_tm + (cc + 2) * kernelTmSize;
+            const float *kernel3_tm = kernel_tm + (cc + 3) * kernelTmSize;
+
+            int q = 0;
+            for(; q + 1 < inChannel; q += 2){
+                const float *k00 = kernel0_tm + q * 64;
+                const float *k01 = kernel0_tm + (q + 1) * 64;
+                const float *k10 = kernel1_tm + q * 64;
+                const float *k11 = kernel1_tm + (q + 1) * 64;
+                const float *k20 = kernel2_tm + q * 64;
+                const float *k21 = kernel2_tm + (q + 1) * 64;
+                const float *k30 = kernel3_tm + q * 64;
+                const float *k31 = kernel3_tm + (q + 1) * 64;
+
+                for(int i = 0; i < 16; i++){
+                    for(int j = 0; j < 4; j++){
+                        ktm2[0 + j] = k00[j];
+                        ktm2[4 + j] = k01[j];
+                        ktm2[8 + j] = k10[j];
+                        ktm2[12 + j] = k11[j];
+                        ktm2[16 + j] = k20[j];
+                        ktm2[20 + j] = k21[j];
+                        ktm2[24 + j] = k30[j];
+                        ktm2[28 + j] = k31[j];
+                    }
+
+                    k00 += 4;
+                    k01 += 4;
+                    k10 += 4;
+                    k11 += 4;
+                    k20 += 4;
+                    k21 += 4;
+                    k30 += 4;
+                    k31 += 4;
+                    ktm2 += 32;
+
+                }
+            }
+
+            //inChannel方向的拖尾部分
+            for(; q < inChannel; q++){
+                const float *k00 = kernel0_tm + q * 64;
+                const float *k10 = kernel1_tm + q * 64;
+                const float *k20 = kernel2_tm + q * 64;
+                const float *k30 = kernel3_tm + q * 64;
+
+                for(int i = 0; i < 16; i++){
+                    for(int j = 0; j < 4; j++){
+                        ktm2[0 + j] = k00[j];
+                        ktm2[4 + j] = k10[j];
+                        ktm2[8 + j] = k20[j];
+                        ktm2[12 + j] = k30[j];
+                    }
+
+                    k00 += 4;
+                    k10 += 4;
+                    k20 += 4;
+                    k30 += 4;
+                    ktm2 += 16;
+                }
+            }
+
+        }
+
+#if USE_OMP
+    #pragma omp parallel for num_threads(OMP_THREAD)
+#endif      
+        for(int cc = remainOutChannel; cc < outChannel; cc++){
+            float *ktm2 = kernel_tm2  + nnOutchannel * packOutH * packOutW + 8 * 8 * inChannel * (cc - remainOutChannel);
+
+            const float* kernel0_tm = kernel_tm + cc * kernelTmSize;
+
+            int q = 0;
+            for(; q < inChannel; q++){
+                const float* k00 = kernel0_tm + q * 64;
+                for(int i = 0; i < 16; i++){
+                    for(int j = 0; j < 4; j++){
+                        ktm2[j] = k00[j];
+                    }
+                    k00 += 4;
+                    ktm2 += 4;
+                }
+            }
+        }        
+
+        kernel_tm = kernel_tm2;
 
     }
 
+    // F(6x6, 3x3) <=> input: 8x8, weight: 8x8
     void ConvolutionalLayerArm3x3s1Winograd::conv3x3s1WinogradNeon(float *const &src, const int &inWidth, const int &inHeight,  const int &inChannel, float *const &kernel,
                                  float* &dest, const int &outWidth, const int &outHeight, const int &outChannel){
+        
+        // Vc,b = B^Td_{c,b}B
+        
+        int outW = (outWidth + 5) / 6 * 6;
+        int outH = (outHeight + 5) / 6 * 6;
+
+        int W = outW + 2;
+        int H = outH + 2;
+        int Top = 0;
+        int Left = 0;
+        int Bottom = H;
+        int Right = W;
+        int PadHeight = Bottom - Top;
+        int PadWidth = Right - Left;
+        float *srcPadding = new float[PadHeight * PadWidth * inChannel];
+        PaddingLayerArm now;
+        now.padding(src, inWidth, inHeight, inChannel, srcPadding, Top, Bottom, Left, Right, 0);
+        
+        int w_tm = outW / 6 * 8;
+        int h_tm = outH / 6 * 8;
+        int src_tm_channel = inChannel;
+        int src_tm_h = 16 * w_tm / 8 * h_tm / 8;
+        int src_tm_w = 4;
+        float *src_tm  = new float[src_tm_channel * src_tm_h * src_tm_w];
+        
+
         return;
     }
 }
