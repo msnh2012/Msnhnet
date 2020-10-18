97ea6e613cf29879ab914edc1c72e5c78bc62dff
BBuf <1182563586@qq.com>
2020-10-18 16:54:33 +0800

    Fix MsnhConvolutionDepthwise3x3s2 BUG

--- src/layers/arm/MsnhConvolutionDepthwise3x3s2.cpp
+++ src/layers/arm/MsnhConvolutionDepthwise3x3s2.cpp
@@ -1,4 +1,3 @@
-#define USE_ARM 1
 #ifdef USE_ARM
 #include "Msnhnet/layers/arm/MsnhConvolutionDepthwise3x3s2.h"
 namespace Msnhnet
@@ -28,9 +27,9 @@ namespace Msnhnet
             const float* r3 = src0 + inWidth * 3;
 
 #if USE_NEON
-            float32x4_t k012 = vld1q_f32(k0);
-            float32x4_t k345 = vld1q_f32(k0 + 3);
-            float32x4_t k678 = vld1q_f32(k0 + 6);
+            float32x4_t k012 = vld1q_f32(kernel0);
+            float32x4_t k345 = vld1q_f32(kernel0 + 3);
+            float32x4_t k678 = vld1q_f32(kernel0 + 6);
             k012 = vsetq_lane_f32(0.f, k012, 3);
             k345 = vsetq_lane_f32(0.f, k345, 3);
             k678 = vsetq_lane_f32(0.f, k678, 3);
