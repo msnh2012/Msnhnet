0b4dc8fde4bcaa50cd8deb9c422889ae73f094ad
BBuf <1182563586@qq.com>
2020-11-07 19:53:01 +0800

    Update IN 2020/11/7

--- src/layers/arm/MsnhConvolution1x1.cpp
+++ src/layers/arm/MsnhConvolution1x1.cpp
@@ -63,8 +63,132 @@ namespace Msnhnet
 #else
                 if(nn > 0){
                     asm volatile(
+                        "0:                                 \n"
+
+                        // float *destptr0 = dest0;
+                        "pld        [%1, #256]              \n"
+                        "vld1.f32   {d16-d19}, [%1]         \n"
+
+                        // float *destptr1 = dest1;
+                        "pld        [%2, #256]              \n"
+                        "vld1.f32   {d20-d23}, [%2]         \n"
+                        // float *destptr2 = dest2;
+                        "pld        [%3, #256]              \n"
+                        "vld1.f32   {d24-d27}, [%3]         \n"
+
+                        // float *destptr3 = dest3;
+                        "pld        [%4, #256]              \n"
+                        "vld1.f32   {d28-d31}, [%4]         \n"
+
+                        // const float *r0 = src0;
+                        "pld        [%5, #256]              \n"
+                        "vld1.f32   {d12-d15}, [%5]!        \n"
                         
-                    
+                        // float sum0 = *r0 * kernel0[0]
+                        "vmla.f32   q8, q6, %e18[0]         \n"
+                        // float sum0_next = *r0 * kernel0[0]
+                        "vmla.f32   q9, q7, %e18[0]         \n"
+
+                        // float sum1 = *r0 * kernel1[0]
+                        "vmla.f32   q10, q6, %e19[0]        \n"
+                        // float sum1_next = *r0 * kernel1[0]
+                        "vmla.f32   q11, q7, %e19[0]        \n"
+
+                        // float sum2 = *r0 * kernel2[0]
+                        "vmla.f32   q12, q6, %e20[0]        \n"
+                        // float sum2_next = *r0 * kernel2[0]
+                        "vmla.f32   q13, q7, %e20[0]        \n"
+
+                        // float sum3 = *r0 * kernel3[0]
+                        "vmla.f32   q14, q6, %e21[0]        \n"
+                        // float sum3_next = *r0 * kernel3[0]
+                        "vmla.f32   q15, q7, %e21[0]        \n"
+                        
+                        // const float *r1 = src1;
+                        "pld        [%6, #256]              \n"
+                        "vld1.f32   {d8-d11}, [%6]!         \n"
+
+
+                        // float sum0 = *r0 * kernel0[0] + *r1 * kernel0[1]
+                        "vmla.f32   q8, q4, %e18[1]         \n"
+                        // float sum0_next = *r0 * kernel0[0] + *r1 * kernel0[1]
+                        "vmla.f32   q9, q5, %e18[1]         \n"
+
+                        // float sum1 = *r0 * kernel1[0] + *r1 * kernel1[1]
+                        "vmla.f32   q10, q4, %e19[1]        \n"
+                        // float sum1_next = *r0 * kernel1[0] + *r1 * kernel1[1]
+                        "vmla.f32   q11, q5, %e19[1]        \n"
+
+                        // float sum2 = *r0 * kernel2[0] + *r1 * kernel2[1]
+                        "vmla.f32   q12, q4, %e20[1]        \n"
+                        // float sum2_next = *r0 * kernel2[0] + *r1 * kernel2[1]
+                        "vmla.f32   q13, q5, %e20[1]        \n"
+
+                        // float sum3 = *r0 * kernel3[0] + *r1 * kernel3[1]
+                        "vmla.f32   q14, q4, %e21[1]        \n"
+                        // float sum3_next = *r0 * kernel3[0] + *r1 * kernel3[1]
+                        "vmla.f32   q15, q5, %e21[1]        \n"
+
+                        // const float *r2 = src2;
+                        "pld        [%7, #256]              \n"
+                        "vld1.f32   {d12-d15}, [%7]!        \n"
+
+                        // float sum0 = *r0 * kernel0[0] + *r1 * kernel0[1] + *r2 * kernel0[2]
+                        "vmla.f32   q8, q6, %f18[0]         \n"
+                        // float sum0_next = *r0 * kernel0[0] + *r1 * kernel0[1] + *r2 * kernel0[2]
+                        "vmla.f32   q9, q7, %f18[0]         \n"
+
+                        //  float sum1 = *r0 * kernel1[0] + *r1 * kernel1[1] + *r2 * kernel1[2]
+                        "vmla.f32   q10, q6, %f19[0]        \n"
+                        //  float sum1_next = *r0 * kernel1[0] + *r1 * kernel1[1] + *r2 * kernel1[2]
+                        "vmla.f32   q11, q7, %f19[0]        \n"
+
+                        // float sum2 = *r0 * kernel2[0] + *r1 * kernel2[1] + *r2 * kernel2[2]
+                        "vmla.f32   q12, q6, %f20[0]        \n"
+                        // float sum2_next = *r0 * kernel2[0] + *r1 * kernel2[1] + *r2 * kernel2[2]
+                        "vmla.f32   q13, q7, %f20[0]        \n"
+
+                        // float sum3 = *r0 * kernel3[0] + *r1 * kernel3[1] + *r2 * kernel3[2]
+                        "vmla.f32   q14, q6, %f21[0]        \n"
+                        // float sum3_next = *r0 * kernel3[0] + *r1 * kernel3[1] + *r2 * kernel3[2]
+                        "vmla.f32   q15, q7, %f21[0]        \n"
+
+                        // const float *r3 = src3;
+                        "pld        [%8, #256]              \n"
+                        "vld1.f32   {d8-d11}, [%8]!         \n"
+
+                        // float sum0 = *r0 * kernel0[0] + *r1 * kernel0[1] + *r2 * kernel0[2] + *r3 * kernel0[3];
+                        "vmla.f32   q8, q4, %f18[1]         \n"
+                        // float sum0_next = *r0 * kernel0[0] + *r1 * kernel0[1] + *r2 * kernel0[2] + *r3 * kernel0[3];
+                        "vmla.f32   q9, q5, %f18[1]         \n"
+
+                        // float sum1 = *r0 * kernel1[0] + *r1 * kernel1[1] + *r2 * kernel1[2] + *r3 * kernel1[3];
+                        "vmla.f32   q10, q4, %f19[1]        \n"
+                        // float sum1_next = *r0 * kernel1[0] + *r1 * kernel1[1] + *r2 * kernel1[2] + *r3 * kernel1[3];
+                        "vmla.f32   q11, q5, %f19[1]        \n"
+
+                        // float sum2 = *r0 * kernel2[0] + *r1 * kernel2[1] + *r2 * kernel2[2] + *r3 * kernel2[3];
+                        "vmla.f32   q12, q4, %f20[1]        \n"
+                        // float sum2_next = *r0 * kernel2[0] + *r1 * kernel2[1] + *r2 * kernel2[2] + *r3 * kernel2[3];
+                        "vmla.f32   q13, q5, %f20[1]        \n"
+
+                        // float sum3 = *r0 * kernel3[0] + *r1 * kernel3[1] + *r2 * kernel3[2] + *r3 * kernel3[3];
+                        "vmla.f32   q14, q4, %f21[1]        \n"
+                        // float sum3_next = *r0 * kernel3[0] + *r1 * kernel3[1] + *r2 * kernel3[2] + *r3 * kernel3[3];
+                        "vmla.f32   q15, q5, %f21[1]        \n"
+
+                        // *destptr0 += sum0;
+                        "vst1.f32   {d16-d19}, [%1]!        \n"
+                        // *destptr1 += sum1;
+                        "vst1.f32   {d20-d23}, [%2]!        \n"
+                        // *destptr2 += sum2;
+                        "vst1.f32   {d24-d27}, [%3]!        \n"
+                        // *destptr3 += sum3;
+                        "vst1.f32   {d28-d31}, [%4]!        \n"
+
+                        // nn-=1
+                        "subs       %0, #1                  \n"
+                        "bne        0b                      \n"
 
                         : "=r"(nn),      // %0
                         "=r"(destptr0), // %1
@@ -207,7 +331,31 @@ namespace Msnhnet
 #else
                 int remain = out_size;
 #endif
-                
+
+#if USE_NEON
+#if __aarch64__
+                throw Exception(1, "Error: armv8 temporarily not supported!", __FILE__, __LINE__, __FUNCTION__);
+#else
+                if(nn > 0){
+                    asm volatile(
+
+                    );
+                }
+#endif
+#endif
+
+                for(; remain > 0; remain--){
+                    float sum0 = *r0 * kernel0[0] + *r1 * kernel0[1] + *r2 * kernel0[2] + *r3 * kernel0[3];
+
+                    *destptr0 += sum0;
+
+                    r0++;
+                    r1++;
+                    r2++;
+                    r3++;
+                    destptr0++;
+                }
+
             }
         }
     }    
