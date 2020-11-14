570a2c34d77daba261e0e92ee45122706f117da5
BBuf <1182563586@qq.com>
2020-11-14 17:56:09 +0800

    Add caffe2Msnhnet tools

---
+++ tools/caffe2Msnhnet/caffe2Msnhnet.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8
+# from pytorch2caffe import plot_graph, pytorch2caffe
+import sys
+import cv2
+import caffe
+import numpy as np
+import os
+from caffenet import *
+import argparse
+import torch
+from PytorchToMsnhnet import *
+
+################################################################################################   
+parser = argparse.ArgumentParser(description='Convert Caffe model to MsnhNet model.',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--model', type=str, default=None)
+parser.add_argument('--weights', type=str, default=None)
+parser.add_argument('--height', type=str, default=None)
+parser.add_argument('--width', type=str, default=None)
+parser.add_argument('--channels', type=str, default=None)
+
+args = parser.parse_args()
+
+model_def = args.model
+model_weights = args.weights
+model_name = model_weights.split('/')[-1].split('.')[0]
+width = args.width
+height = args.height
+channels = args.channels
+
+
+net = CaffeNet(model_def, width=width, height=height, channels=channels)
+net.load_weights(model_weights)
+net.to('cpu')
+net.eval()
+
+input=torch.ones([1,channels,height,width])
+
+trans(net, input,"model_name.msnhnet","model_name.msnhbin")
+
+
