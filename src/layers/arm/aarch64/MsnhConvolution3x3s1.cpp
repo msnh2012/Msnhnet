2c41df243604419b6b9d692a9428ce4a144c0511
BBuf <1182563586@qq.com>
2020-12-23 22:55:07 +0800

    add: MsnhConvolution3x3s1.cpp

--- src/layers/arm/aarch64/MsnhConvolution3x3s1.cpp
+++ src/layers/arm/aarch64/MsnhConvolution3x3s1.cpp
@@ -177,7 +177,8 @@ void ConvolutionalLayerArmV8_3x3s1::conv3x3s1Neon(float *const &src, const int &
                         // v10.4s [b1, c1, d1, e1] 和k012_next的第二个元素相乘并累加到v13.4s
                         "fmla   v13.4s, v10.4s, %21.s[1]    \n"
 
-                        // 
+                        // v8.4s [a2, b2, c2, d2]
+                        // v9.4s [e2, f2, g2, h2]
                         "prfm   pldl1keep, [%7, #256]       \n"
                         "ld1    {v8.4s, v9.4s}, [%7]        \n" // r2
                         "add    %7, %7, #16                 \n"
@@ -191,6 +192,9 @@ void ConvolutionalLayerArmV8_3x3s1::conv3x3s1Neon(float *const &src, const int &
                         // v11.4s [c1, d1, e1, f1] 和k012_next的第三个元素相乘并累加到v12.4s
                         "fmla   v13.4s, v11.4s, %21.s[2]    \n"
 
+                        // v8.4s [a2, b2, c2, d2]
+                        // v9.4s [e2, f2, g2, h2]
+                        // v10.4s [b2, c2, d2, e2]
                         "ext    v10.16b, v8.16b, v9.16b, #4 \n"
 
                         "fmla   v6.4s, v8.4s, %20.s[0]      \n"
@@ -198,6 +202,9 @@ void ConvolutionalLayerArmV8_3x3s1::conv3x3s1Neon(float *const &src, const int &
                         "fmla   v12.4s, v8.4s, %19.s[0]     \n"
                         "fmla   v13.4s, v8.4s, %22.s[0]     \n"
 
+                        // v8.4s [a2, b2, c2, d2]
+                        // v9.4s [e2, f2, g2, h2]
+                        // v10.4s [c2, d2, e2, f2]
                         "ext    v11.16b, v8.16b, v9.16b, #8 \n"
 
                         "fmla   v6.4s, v10.4s, %20.s[1]     \n"
@@ -205,6 +212,7 @@ void ConvolutionalLayerArmV8_3x3s1::conv3x3s1Neon(float *const &src, const int &
                         "fmla   v12.4s, v10.4s, %19.s[1]    \n"
                         "fmla   v13.4s, v10.4s, %22.s[1]    \n"
 
+                        //
                         "prfm   pldl1keep, [%5, #256]       \n"
                         "ld1    {v8.4s, v9.4s}, [%5]        \n" // r0
                         "add    %5, %5, #16                 \n"
@@ -399,7 +407,104 @@ void ConvolutionalLayerArmV8_3x3s1::conv3x3s1Neon(float *const &src, const int &
 
 #if USE_NEON
 
+                asm volatile(
+                        "0:                                 \n"
 
+                        // v8.4s [a0, b0, c0, d0]
+                        // v9.4s [e0, f0, g0, h0]
+                        "prfm   pldl1keep, [%3, #256]       \n"
+                        "ld1    {v8.4s, v9.4s}, [%3]        \n" // r0
+                        "add    %3, %3, #16                 \n"
+
+                        // sum0
+                        "prfm   pldl1keep, [%1, #128]       \n"
+                        "ld1    {v6.4s}, [%1]               \n" 
+
+                        //sum1
+                        "prfm   pldl1keep, [%2, #128]       \n"
+                        "ld1    {v7.4s}, [%2]               \n"
+
+                        // v8.4s [a0, b0, c0, d0] 和k012的第一个元素相乘获得v14.4s
+                        "fmul   v14.4s, v8.4s, %12.s[0]     \n"
+                        // v8.4s [a0, b0, c0, d0] 和k012_next的的第一个元素相乘获得v15.4s
+                        "fmul   v15.4s, v8.4s, %15.s[0]     \n"
+
+                        // v8.4s [a0, b0, c0, d0]
+                        // v9.4s [e0, f0, g0, h0]
+                        // v10.4s [b0, c0, d0, e0]
+                        "ext    v10.16b, v8.16b, v9.16b, #4 \n"
+                        // v11.4s [c0, d0, e0, f0]
+                        "ext    v11.16b, v8.16b, v9.16b, #8 \n"
+
+                        // v10.4s [b0, c0, d0, e0]和k012的第二个元素相乘并累加到v6.4s
+                        "fmla   v6.4s, v10.4s, %12.s[1]     \n"
+                        // v10.4s [b0, c0, d0, e0]和k012_next的第二个元素相乘并累加到v7.4s
+                        "fmla   v7.4s, v10.4s, %15.s[1]     \n"
+
+                        "prfm   pldl1keep, [%4, #256]       \n"
+                        "ld1    {v8.4s, v9.4s}, [%4]        \n" // r1
+                        "add    %4, %4, #16                 \n"
+
+                        "fmla   v14.4s, v11.4s, %12.s[2]    \n"
+                        "fmla   v15.4s, v11.4s, %15.s[2]    \n"
+
+                        "fmla   v6.4s, v8.4s, %13.s[0]      \n"
+                        "fmla   v7.4s, v8.4s, %16.s[0]      \n"
+
+                        "ext    v10.16b, v8.16b, v9.16b, #4 \n"
+                        "ext    v11.16b, v8.16b, v9.16b, #8 \n"
+
+                        "fmla   v14.4s, v10.4s, %13.s[1]    \n"
+                        "fmla   v15.4s, v10.4s, %16.s[1]    \n"
+
+                        "prfm   pldl1keep, [%5, #256]       \n"
+                        "ld1    {v8.4s, v9.4s}, [%5]        \n" // r2
+                        "add    %5, %5, #16                 \n"
+
+                        "fmla   v6.4s, v11.4s, %13.s[2]     \n"
+                        "fmla   v7.4s, v11.4s, %16.s[2]     \n"
+
+                        "fmla   v14.4s, v8.4s, %14.s[0]     \n"
+                        "fmla   v15.4s, v8.4s, %17.s[0]     \n"
+
+                        "ext    v10.16b, v8.16b, v9.16b, #4 \n"
+                        "ext    v11.16b, v8.16b, v9.16b, #8 \n"
+
+                        "fmla   v6.4s, v10.4s, %14.s[1]     \n"
+                        "fmla   v7.4s, v10.4s, %17.s[1]     \n"
+
+                        "fmla   v14.4s, v11.4s, %14.s[2]    \n"
+                        "fmla   v15.4s, v11.4s, %17.s[2]    \n"
+
+                        "fadd   v6.4s, v6.4s, v14.4s        \n"
+                        "fadd   v7.4s, v7.4s, v15.4s        \n"
+
+                        "st1    {v6.4s}, [%1], #16          \n"
+                        "st1    {v7.4s}, [%2], #16          \n"
+
+                        "subs   %w0, %w0, #1                \n"
+                        "bne    0b                          \n"
+
+                        : "=r"(nn),      // %0
+                        "=r"(destptr0), // %1
+                        "=r"(destptr1), // %2
+                        "=r"(r0),      // %3
+                        "=r"(r1),      // %4
+                        "=r"(r2)       // %5
+                        : "0"(nn),
+                        "1"(destptr0),
+                        "2"(destptr1),
+                        "3"(r0),
+                        "4"(r1),
+                        "5"(r2),
+                        "w"(k012), // %12
+                        "w"(k345), // %13
+                        "w"(k678), // %14
+                        "w"(k012_next), // %15
+                        "w"(k345_next), // %16
+                        "w"(k678_next)  // %17
+                        : "cc", "memory", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15");
+                }
 
 #endif
 
@@ -519,8 +624,111 @@ void ConvolutionalLayerArmV8_3x3s1::conv3x3s1Neon(float *const &src, const int &
 #endif
 
 #if USE_NEON
+                if(nn > 0){
+                    asm volatile(
+                        "prfm   pldl1keep, [%3, #256]       \n"
+                        "ld1    {v9.4s, v10.4s}, [%3]       \n" // r0
+                        "add    %3, %3, #16                 \n"
 
+                        "ext    v11.16b, v9.16b, v10.16b, #4 \n"
+                        "ext    v12.16b, v9.16b, v10.16b, #8 \n"
 
+                        "0:                                 \n"
+
+                        "prfm   pldl1keep, [%1, #128]       \n"
+                        "ld1    {v7.4s}, [%1]               \n" // _sum
+
+                        "fmla   v7.4s, v9.4s, %14.s[0]      \n"
+                        "fmul   v6.4s, v11.4s, %14.s[1]     \n"
+                        "fmul   v13.4s, v12.4s, %14.s[2]    \n"
+
+                        "prfm   pldl1keep, [%4, #256]       \n"
+                        "ld1    {v9.4s, v10.4s}, [%4]       \n" // r1
+                        "add    %4, %4, #16                 \n"
+
+                        "fmla   v7.4s, v9.4s, %15.s[0]      \n"
+
+                        "ext    v11.16b, v9.16b, v10.16b, #4 \n"
+                        "ext    v12.16b, v9.16b, v10.16b, #8 \n"
+
+                        "fmla   v6.4s, v11.4s, %15.s[1]     \n"
+                        "fmla   v13.4s, v12.4s, %15.s[2]    \n"
+
+                        "prfm   pldl1keep, [%2, #128]       \n"
+                        "ld1    {v8.4s}, [%2]               \n" // _sum2
+
+                        "fmla   v8.4s, v9.4s, %14.s[0]      \n"
+                        "fmul   v14.4s, v11.4s, %14.s[1]    \n"
+                        "fmul   v15.4s, v12.4s, %14.s[2]    \n"
+
+                        "prfm   pldl1keep, [%5, #256]       \n"
+                        "ld1    {v9.4s, v10.4s}, [%5]       \n" // r2
+                        "add    %5, %5, #16                 \n"
+
+                        "fmla   v7.4s, v9.4s, %16.s[0]      \n"
+
+                        "ext    v11.16b, v9.16b, v10.16b, #4 \n"
+                        "ext    v12.16b, v9.16b, v10.16b, #8 \n"
+
+                        "fmla   v6.4s, v11.4s, %16.s[1]     \n"
+                        "fmla   v13.4s, v12.4s, %16.s[2]    \n"
+
+                        "fmla   v8.4s, v9.4s, %15.s[0]      \n"
+                        "fmla   v14.4s, v11.4s, %15.s[1]    \n"
+                        "fmla   v15.4s, v12.4s, %15.s[2]    \n"
+
+                        "prfm   pldl1keep, [%6, #256]       \n"
+                        "ld1    {v9.4s, v10.4s}, [%6]       \n" // r3
+                        "add    %6, %6, #16                 \n"
+
+                        "fmla   v8.4s, v9.4s, %16.s[0]      \n"
+
+                        "ext    v11.16b, v9.16b, v10.16b, #4 \n"
+                        "ext    v12.16b, v9.16b, v10.16b, #8 \n"
+
+                        "fmla   v14.4s, v11.4s, %16.s[1]    \n"
+                        "fmla   v15.4s, v12.4s, %16.s[2]    \n"
+
+                        "fadd   v7.4s, v7.4s, v6.4s         \n"
+
+                        "prfm   pldl1keep, [%3, #256]       \n"
+                        "ld1    {v9.4s, v10.4s}, [%3]       \n" // r0
+
+                        "fadd   v8.4s, v8.4s, v14.4s        \n"
+                        "fadd   v7.4s, v7.4s, v13.4s        \n"
+                        "fadd   v8.4s, v8.4s, v15.4s        \n"
+
+                        "ext    v11.16b, v9.16b, v10.16b, #4 \n"
+                        "ext    v12.16b, v9.16b, v10.16b, #8 \n"
+
+                        "add    %3, %3, #16                 \n"
+
+                        "st1    {v7.4s}, [%1], #16          \n"
+                        "st1    {v8.4s}, [%2], #16          \n"
+
+                        "subs   %w0, %w0, #1                \n"
+                        "bne    0b                          \n"
+
+                        "sub    %3, %3, #16                 \n"
+                        : "=r"(nn),      // %0
+                        "=r"(destptr0),  // %1
+                        "=r"(destptr1), // %2
+                        "=r"(r0),      // %3
+                        "=r"(r1),      // %4
+                        "=r"(r2),      // %5
+                        "=r"(r3)       // %6
+                        : "0"(nn),
+                        "1"(destptr0),
+                        "2"(destptr1),
+                        "3"(r0),
+                        "4"(r1),
+                        "5"(r2),
+                        "6"(r3),
+                        "w"(k012), // %14
+                        "w"(k345), // %15
+                        "w"(k678)  // %16
+                        : "cc", "memory", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15");
+                }
 #endif
 
                 for(; remain > 0; remain--){
@@ -596,7 +804,79 @@ void ConvolutionalLayerArmV8_3x3s1::conv3x3s1Neon(float *const &src, const int &
 #endif
 
 #if USE_NEON
+                if(nn > 0){
+                    asm volatile(
+                        "prfm   pldl1keep, [%2, #256]       \n"
+                        "ld1    {v8.4s, v9.4s}, [%2]        \n" // r0
+                        "add    %2, %2, #16                 \n"
 
+                        "ext    v10.16b, v8.16b, v9.16b, #4 \n"
+                        "ext    v11.16b, v8.16b, v9.16b, #8 \n"
+
+                        "0:                                 \n"
+
+                        "prfm   pldl1keep, [%1, #128]       \n"
+                        "ld1    {v7.4s}, [%1]               \n" // _sum
+
+                        "fmla   v7.4s, v8.4s, %10.s[0]      \n"
+                        "fmul   v13.4s, v10.4s, %10.s[1]    \n"
+                        "fmul   v14.4s, v11.4s, %10.s[2]    \n"
+
+                        "prfm   pldl1keep, [%3, #256]       \n"
+                        "ld1    {v8.4s, v9.4s}, [%3]        \n" // r1
+                        "add    %3, %3, #16                 \n"
+
+                        "fmla   v7.4s, v8.4s, %11.s[0]      \n"
+
+                        "ext    v10.16b, v8.16b, v9.16b, #4 \n"
+                        "ext    v11.16b, v8.16b, v9.16b, #8 \n"
+
+                        "fmla   v13.4s, v10.4s, %11.s[1]    \n"
+                        "fmla   v14.4s, v11.4s, %11.s[2]    \n"
+
+                        "prfm   pldl1keep, [%4, #256]       \n"
+                        "ld1    {v8.4s, v9.4s}, [%4]        \n" // r2
+                        "add    %4, %4, #16                 \n"
+
+                        "fmla   v7.4s, v8.4s, %12.s[0]      \n"
+
+                        "ext    v10.16b, v8.16b, v9.16b, #4 \n"
+                        "ext    v11.16b, v8.16b, v9.16b, #8 \n"
+
+                        "fmla   v13.4s, v10.4s, %12.s[1]    \n"
+                        "fmla   v14.4s, v11.4s, %12.s[2]    \n"
+
+                        "prfm   pldl1keep, [%2, #256]       \n"
+                        "ld1    {v8.4s, v9.4s}, [%2]        \n" // r0
+                        "add    %2, %2, #16                 \n"
+
+                        "fadd   v7.4s, v7.4s, v13.4s        \n"
+                        "fadd   v7.4s, v7.4s, v14.4s        \n"
+
+                        "ext    v10.16b, v8.16b, v9.16b, #4 \n"
+                        "ext    v11.16b, v8.16b, v9.16b, #8 \n"
+
+                        "st1    {v7.4s}, [%1], #16          \n"
+
+                        "subs   %w0, %w0, #1                \n"
+                        "bne    0b                          \n"
+
+                        "sub    %2, %2, #16                 \n"
+                        : "=r"(nn),     // %0
+                        "=r"(destptr0), // %1
+                        "=r"(r0),     // %2
+                        "=r"(r1),     // %3
+                        "=r"(r2)      // %4
+                        : "0"(nn),
+                        "1"(destptr0),
+                        "2"(r0),
+                        "3"(r1),
+                        "4"(r2),
+                        "w"(k012), // %10
+                        "w"(k345), // %11
+                        "w"(k678)  // %12
+                        : "cc", "memory", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15");
+                }
 
 #endif
 
