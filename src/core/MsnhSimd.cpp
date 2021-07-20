95b86d02cf1cf77af945bd893203fac32477bfd4
msnh2012 <wwwtyyz@outlook.com>
2021-07-20 17:31:49 +0800

    Fix arm bug

--- src/core/MsnhSimd.cpp
+++ src/core/MsnhSimd.cpp
@@ -2,6 +2,7 @@
 
 namespace Msnhnet
 {
+#ifdef USE_X86
 bool SimdInfo::supportSSE    = false;
 bool SimdInfo::supportSSE2   = false;
 bool SimdInfo::supportSSE3   = false;
@@ -13,4 +14,5 @@ bool SimdInfo::supportAVX    = false;
 bool SimdInfo::supportAVX2   = false;
 bool SimdInfo::supportAVX512 = false;
 bool SimdInfo::checked       = false;
+#endif
 }
